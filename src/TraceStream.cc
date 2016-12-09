b64e3067d44ec24891de1a1f83b6d84fbb2b2340
Ravi Shankar <wafflespeanut@gmail.com>
2016-12-10 00:16:34 +0530

    Print the trace directory regardless of interactive mode

--- src/TraceStream.cc
+++ src/TraceStream.cc
@@ -643,10 +643,9 @@ TraceWriter::TraceWriter(const std::string& file_name, int bind_to_cpu)
   }
   unlink(version_clone_path.c_str());
 
-  if (!probably_not_interactive(STDOUT_FILENO)) {
-    printf("rr: Saving execution to trace directory `%s'.\n",
-           trace_dir.c_str());
-  }
+  printf("rr: Saving execution to trace directory `%s'.\n",
+         trace_dir.c_str());
+  fflush(stdout);
 
   write_generic(&bind_to_cpu, sizeof(bind_to_cpu));
 }
